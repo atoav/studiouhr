9e8e2f2bce779bc9aa2cf9399bfbff325bcbc735
David Huss <dh@atoav.com>
2018-01-22 21:37:12 +0100

    Fixed path variables, quiet mode

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+STARTDIR=pwd
 sudo pip install virtualenv
 virtualenv .env
 source .env/bin/activate
 pip install .
 cd /usr/local/bin
 sudo ln -s ~/studiouhr/.env/bin/studiouhr
-cd -
+cd $STARTDIR
 source .env/bin/activate
 pip install --upgrade .
--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-git fetch origin master
-git reset --hard FETCH_HEAD
-git clean -df
+git fetch -q origin master
+git reset -q --hard FETCH_HEAD
+git clean -q -df
 source .env/bin/activate
 pip install --upgrade .
