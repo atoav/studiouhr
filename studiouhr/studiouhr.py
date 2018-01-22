f5f030aabf6ac3175807e543dfd260b28d2c10d5
David Huss <dh@atoav.com>
2018-01-22 20:25:09 +0100

    Added newlines

--- studiouhr/studiouhr.py
+++ studiouhr/studiouhr.py
@@ -151,10 +151,10 @@ def printstats(value):
     global b
     global hostname
     now = datetime.now()
-    sys.stdout.write("\r"+"-"*15+" studiouhr.py running @"+str(hostname)+" "+"-"*15)
-    sys.stdout.write("\r"+("Time:").ljust(30)+now.strftime("%H:%M:%S.%f"))
-    sys.stdout.write("\r"+"FPS:".ljust(30)+str(pyglet.clock.get_fps()))
-    colorstring = "Current Color:".ljust(30)+str((r,g,b))
+    sys.stdout.write("-"*15+" studiouhr.py running @"+str(hostname)+" "+"-"*15+"\n")
+    sys.stdout.write("\r"+("Time:").ljust(30)+now.strftime("%H:%M:%S.%f"+"\n"))
+    sys.stdout.write("\r"+"FPS:".ljust(30)+str(pyglet.clock.get_fps())+"\n")
+    colorstring = "Current Color:".ljust(30)+str((r,g,b)+"\n")
     sys.stdout.write("\r"+colorstring)
     sys.stdout.flush()
 
