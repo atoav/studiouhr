029f989453ef3a51c24977206fe017b4f89a0886
David Huss <dh@atoav.com>
2018-01-22 17:08:56 +0100

    Fixed entrypoint

--- setup.py
+++ setup.py
@@ -10,6 +10,6 @@ setup(name='studiouhr',
         packages=['studiouhr'],
         install_requires=['pyglet', 'astral'],
         package_data = {'':['fonts/*.ttf']},
-        entry_points={'console_scripts':['studiouhr=studiouhr.studiouhr:main_func']},
+        entry_points={'console_scripts':['studiouhr=studiouhr.studiouhr:main']},
         zip_safe=False
         )
